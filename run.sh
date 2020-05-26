d17f1bad1b6641adab4ae6b4f4dcc2a0ab73aa7d
Chris Birchall <chris.birchall@47deg.com>
2020-05-26 12:53:05 +0100

    Use master branch of mu-scala

--- run.sh
+++ run.sh
@@ -4,11 +4,6 @@ set -eo pipefail
 
 git clone https://github.com/higherkindness/mu-scala.git
 
-# TODO remove this once the integration test updates are merged in mu-scala
-pushd mu-scala
-git checkout mu-haskell-v0.3
-popd
-
 # Overwrite stack.yaml to point at Mu-Haskell master HEAD
 sha=$(curl -s https://api.github.com/repos/higherkindness/mu-haskell/branches/master | jq -j '.commit.sha')
 sed -e "s/HEAD/$sha/" stack.yaml > mu-scala/modules/haskell-integration-tests/mu-haskell-client-server/stack.yaml
